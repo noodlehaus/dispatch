f48e4538c2e6d8096da95fc713e1a02e4457268f
Jesus A. Domingo <jesus.domingo@gmail.com>
2012-02-02 16:48:54 +0800

    Tests to some functions added, more tests needed

--- tests/dispatch-test.php
+++ tests/dispatch-test.php
@@ -1,37 +1,41 @@
 <?php
-test_case('method()', function () {
+Suite::name('dispatch php micro framework tests');
+
+Suite::add('method()', function () {
 	$_SERVER['REQUEST_METHOD'] = 'post';
 	assert(method() == 'POST');
 	assert(method('POST') == true);
 });
 
-test_case('html()', function () {
+Suite::add('html()', function () {
 	assert('&amp;' == html('&'));
 	assert('&quot;' == html('"'));
 });
 
-test_case('from()', function () {
+Suite::add('from()', function () {
 	$data = array('name' => 'jaydee', 'age' => 32, 'location' => 'singapore');
 	assert(from($data, 'name') == $data['name']);
 	assert(from($data, array('name', 'age')) == array('name' => $data['name'], 'age' => $data['age']));
 });
 
-test_case('stash()', function () {
+Suite::add('stash()', function () {
 	$data = array('name' => 'jaydee');
 	stash('data', $data);
 	assert(stash('data') == $data);
 });
 
-test_case('config()', function () {
+Suite::add('config()', function () {
 	config('views', 'views/');
 	assert(config('views') == 'views/');
 });
 
-test_case('precondition()', function () {
+Suite::add('precondition()', function () {
 	precondition('setup_stash', function () {
 		stash('stash', true);
 	});
 	precondition('setup_stash');
 	assert(stash('stash') == true);
 });
+
+Suite::run();
 ?>
