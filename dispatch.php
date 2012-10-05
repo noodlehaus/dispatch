3320c2841b5cd888984520c6f8afd1c048d44374
noodlehaus <noodlehaus@gmail>
2012-10-06 01:19:13 +0800

    web only, no more pass(), only get/post

--- dispatch.php
+++ dispatch.php
@@ -1,28 +1,12 @@
 <?php
-/**
- * dispatch php 5.3 utility library
- *
- * Jesus A. Domingo
- * http://noodlehaus.github.com/dispatch
- **/
-
-function error($code, $message) {
-  if (PHP_SAPI === 'cli')
-    die("Error {$code}: {$message}\n");
-  else
-    http_error($code, $message);
-}
-
-function http_error($code = 500, $message = "Internal server error") {
-  @header("HTTP/1.0 {$code} {$message}", true, $code);
-  die($message);
-}
-
 if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50300) {
   error(500, 'dispatch requires at least PHP 5.3 to run.');
 }
 
-class PassException extends Exception {}
+function error($code, $message) {
+  @header("HTTP/1.0 {$code} {$message}", true, $code);
+  die($message);
+}
 
 function config($key, $value = null) {
 
@@ -34,12 +18,6 @@ function config($key, $value = null) {
     return (isset($_config[$key]) ? $_config[$key] : null);
 
   $_config[$key] = $value;
-}
-
-function json_response($obj, $code = 200) {
-  header('Content-type: application/json', true, $code);
-  echo json_encode($obj);
-  exit;
 }
 
 function to_b64($str) {
@@ -62,8 +40,8 @@ if (extension_loaded('mcrypt')) {
 
   function encrypt($decoded) {
 
-    if (($secret = config('secret')) == null)
-      error(500, 'encrypt() requires that you define the [secret] setting.');
+    if (($secret = config('cookies.secret')) == null)
+      error(500, '[cookies.secret] is not set');
 
     $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
     $iv_code = mcrypt_create_iv($iv_size, MCRYPT_RAND);
@@ -73,8 +51,8 @@ if (extension_loaded('mcrypt')) {
 
   function decrypt($encoded) {
 
-    if (($secret = config('secret')) == null)
-      error(500, 'decrypt() requires that you define the [application.secret] setting.');
+    if (($secret = config('cookies.secret')) == null)
+      error(500, '[cookies.secret] is not set');
 
     $enc_str = from_b64($encoded);
     $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
@@ -153,67 +131,63 @@ function stash($name, $value = null) {
   return $value;
 }
 
-if (PHP_SAPI !== 'cli') {
+function method($verb = null) {
 
-  function method($verb = null) {
+  if ($verb == null || (strtoupper($verb) == strtoupper($_SERVER['REQUEST_METHOD'])))
+    return strtoupper($_SERVER['REQUEST_METHOD']);
 
-    if ($verb == null || (strtoupper($verb) == strtoupper($_SERVER['REQUEST_METHOD'])))
-      return strtoupper($_SERVER['REQUEST_METHOD']);
+  error(400, 'bad request');
+}
 
-    error(400, 'Bad request');
-  }
+function client_ip() {
 
-  function client_ip() {
+  if (isset($_SERVER['HTTP_CLIENT_IP']))
+    return $_SERVER['HTTP_CLIENT_IP'];
+  else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+    return $_SERVER['HTTP_X_FORWARDED_FOR'];
 
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-      return $_SERVER['HTTP_CLIENT_IP'];
-    else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-      return $_SERVER['HTTP_X_FORWARDED_FOR'];
+  return $_SERVER['REMOTE_ADDR'];
+}
 
-    return $_SERVER['REMOTE_ADDR'];
-  }
+function redirect(/* $code_or_path, $path_or_cond, $cond */) {
 
-  function redirect(/* $code_or_path, $path_or_cond, $cond */) {
+  $argv = func_get_args();
+  $argc = count($argv);
 
-    $argv = func_get_args();
-    $argc = count($argv);
+  $path = null;
+  $code = 302;
+  $cond = true;
 
-    $path = null;
-    $code = 302;
-    $cond = true;
-
-    switch ($argc) {
-      case 3:
-        list($code, $path, $cond) = $argv;
-        break;
-      case 2:
-        if (is_string($argv[0]) ? $argv[0] : $argv[1]) {
-          $code = 302;
-          $path = $argv[0];
-          $cond = $argv[1];
-        } else {
-          $code = $argv[0];
-          $path = $argv[1];
-        }
-        break;
-      case 1:
-        if (!is_string($argv[0]))
-          error(500, 'Incorrect call to redirect()');
+  switch ($argc) {
+    case 3:
+      list($code, $path, $cond) = $argv;
+      break;
+    case 2:
+      if (is_string($argv[0]) ? $argv[0] : $argv[1]) {
+        $code = 302;
         $path = $argv[0];
-        break;
-      default:
-        error(500, 'Incorrect call to redirect()');
-    }
-
-    $cond = (is_callable($cond) ? !!call_user_func($cond) : !!$cond);
-
-    if (!$cond)
-      return;
-
-    header('Location: '.$path, true, $code);
-    exit;
+        $cond = $argv[1];
+      } else {
+        $code = $argv[0];
+        $path = $argv[1];
+      }
+      break;
+    case 1:
+      if (!is_string($argv[0]))
+        error(500, 'bad call to redirect()');
+      $path = $argv[0];
+      break;
+    default:
+      error(500, 'bad call to redirect()');
   }
 
+  $cond = (is_callable($cond) ? !!call_user_func($cond) : !!$cond);
+
+  if (!$cond)
+    return;
+
+  header('Location: '.$path, true, $code);
+  exit;
 }
 
 function partial($view, $locals = null) {
@@ -222,8 +196,8 @@ function partial($view, $locals = null) {
     extract($locals, EXTR_SKIP);
   }
 
-  $view_root = config('views');
-  $view_root = ($view_root == null) ? './views' : $view_root;
+  if (($view_root = config('views.root')) == null)
+    error(500, "[views.root] is not set");
 
   $path = basename($view);
   $view = preg_replace('/'.$path.'$/', "_{$path}", $view);
@@ -234,14 +208,14 @@ function partial($view, $locals = null) {
     require $view;
     return ob_get_clean();
   } else {
-    error(500, "Partial [{$view}] not found");
+    error(500, "partial [{$view}] not found");
   }
 
   return '';
 }
 
 function content($value = null) {
-  return stash('__content__', $value);
+  return stash('$content$', $value);
 }
 
 function render($view, $locals = null, $layout = null) {
@@ -250,8 +224,8 @@ function render($view, $locals = null, $layout = null) {
     extract($locals, EXTR_SKIP);
   }
 
-  $view_root = config('views');
-  $view_root = ($view_root == null) ? './views' : $view_root;
+  if (($view_root = config('views.root')) == null)
+    error(500, "[views.root] is not set");
 
   ob_start();
   include "{$view_root}/{$view}.html.php";
@@ -260,7 +234,7 @@ function render($view, $locals = null, $layout = null) {
   if ($layout !== false) {
 
     if ($layout == null) {
-      $layout = config('layout');
+      $layout = config('views.layout');
       $layout = ($layout == null) ? 'layout' : $layout;
     }
 
@@ -277,27 +251,33 @@ function render($view, $locals = null, $layout = null) {
   }
 }
 
+function json($obj, $code = 200) {
+  header('Content-type: application/json', true, $code);
+  echo json_encode($obj);
+  exit;
+}
+
 function condition() {
 
-  static $cb = array();
+  static $cb_map = array();
 
   $argv = func_get_args();
   $argc = count($argv);
 
   if (!$argc)
-    error(500, 'Incorrect call to condition()');
+    error(500, 'bad call to condition()');
 
   $name = array_shift($argv);
   $argc = $argc - 1;
 
-  if (!$argc && is_callable($cb[$name]))
-    return call_user_func($cb[$name]);
+  if (!$argc && is_callable($cb_map[$name]))
+    return call_user_func($cb_map[$name]);
 
   if (is_callable($argv[0]))
-    return ($cb[$name] = $argv[0]);
+    return ($cb_map[$name] = $argv[0]);
 
-  if (is_callable($cb[$name]))
-    return call_user_func_array($cb[$name], $argv);
+  if (is_callable($cb_map[$name]))
+    return call_user_func_array($cb_map[$name], $argv);
 
   error(500, 'condition ['.$name.'] is undefined');
 }
@@ -333,7 +313,7 @@ function filter($sym, $cb_or_val = null) {
     return;
   }
 
-  error(500, 'Call to filter() requires either a symbol + callback or a list of symbols to filter');
+  error(500, 'bad call to filter()');
 }
 
 function route_to_regex($route) {
@@ -349,80 +329,64 @@ function route($method, $pattern, $callback = null) {
   // callback map by request type
   static $route_map = array(
     'GET' => array(),
-    'POST' => array(),
-    'PUT' => array(),
-    'DELETE' => array()
+    'POST' => array()
   );
 
   $method = strtoupper($method);
 
-  if (in_array($method, array('GET', 'POST', 'PUT', 'DELETE'))) {
+  if (!in_array($method, array('GET', 'POST')))
+    error(500, 'Only GET and POST are supported');
 
-    // a callback was passed, so we create a route defiition
-    if ($callback !== null) {
+  // a callback was passed, so we create a route defiition
+  if ($callback !== null) {
 
-      // create a route entry for this pattern
-      $route_map[$method][$pattern] = array(
-        'expression' => route_to_regex($pattern),
-        'callback' => $callback
-      );
-
-    } else {
-
-      // callback is null, so this is a route invokation. look up the callback.
-      foreach ($route_map[$method] as $pat => $obj) {
-
-        // if the requested uri ($pat) has a matching route, let's invoke the cb
-        if (preg_match($obj['expression'], $pattern, $vals)) {
-
-          // call middleware
-          middleware($pattern);
-
-          // construct the params for the callback
-          array_shift($vals);
-          preg_match_all('@:([\w]+)@', $pat, $keys, PREG_PATTERN_ORDER);
-          $keys = array_shift($keys);
-          $params = array();
-
-          foreach ($keys as $index => $id) {
-            $id = substr($id, 1);
-            if (isset($vals[$id])) {
-              array_push($params, urlencode($vals[$id]));
-            }
-          }
-
-          // call filters if we have symbols
-          if (count($keys)) {
-            filter(array_values($keys), $vals);
-          }
-
-          // if no call to pass was made, exit after first route
-          try {
-            if (is_callable($obj['callback'])) {
-              call_user_func_array($obj['callback'], $params);
-            }
-            break;
-          } catch (PassException $e) {
-            continue;
-          }
-
-        }
-      }
-    }
+    // create a route entry for this pattern
+    $route_map[$method][$pattern] = array(
+      'xp' => route_to_regex($pattern),
+      'cb' => $callback
+    );
 
   } else {
-    error(500, "Request method [{$method}] is not supported.");
-  }
-}
 
-function any($only, $path, $cb) {
-  if (is_string($only) && is_callable($path)) {
-    route($only, $path);
-  } else if (is_array($only) && is_string($path) && is_callable($cb)) {
-    foreach ($only as $method) {
-      route($method, $cb);
+    // callback is null, so this is a route invokation. look up the callback.
+    foreach ($route_map[$method] as $pat => $obj) {
+
+      // if the requested uri ($pat) has a matching route, let's invoke the cb
+      if (!preg_match($obj['xp'], $pattern, $vals))
+        continue;
+
+      // call middleware
+      middleware($pattern);
+
+      // construct the params for the callback
+      array_shift($vals);
+      preg_match_all('@:([\w]+)@', $pat, $keys, PREG_PATTERN_ORDER);
+      $keys = array_shift($keys);
+      $argv = array();
+
+      foreach ($keys as $index => $id) {
+        $id = substr($id, 1);
+        if (isset($vals[$id])) {
+          array_push($argv, $vals[$id]);
+        }
+      }
+
+      // call filters if we have symbols
+      if (count($keys)) {
+        filter(array_values($keys), $vals);
+      }
+
+      // if cb found, invoke it
+      if (is_callable($obj['cb'])) {
+        call_user_func_array($obj['cb'], $argv);
+      }
+
+      // leave after first match
+      break;
+
     }
   }
+
 }
 
 function get($path, $cb) {
@@ -433,24 +397,12 @@ function post($path, $cb) {
   route('POST', $path, $cb);
 }
 
-function put($path, $cb) {
-  route('PUT', $path, $cb);
-}
-
-function del($path, $cb) {
-  route('DELETE', $path, $cb);
-}
-
-function pass() {
-  throw new PassException('Jumping to pass handler');
-}
-
 function flash($key, $msg = null, $now = false) {
 
   static $x = array(),
          $f = null;
 
-  $f = (config('f_cookie') ? config('f_cookie') : '_F');
+  $f = (config('cookies.flash') ? config('cookies.flash') : '_F');
 
   if ($c = get_cookie($f))
     $c = json_decode($c, true);
@@ -476,20 +428,13 @@ function flash($key, $msg = null, $now = false) {
   $x[$key] = $msg;
 }
 
-function dispatch($fake_uri = null, $fake_method = null) {
+function dispatch() {
 
-  if (PHP_SAPI === 'cli' && (!$fake_uri || !$fake_method))
-    error(500, 'dispatch() requires a fake uri and method when invoked from the CLI');
-
-  $parts = preg_split('/\?/', ($fake_uri == null ? $_SERVER['REQUEST_URI'] : $fake_uri), -1, PREG_SPLIT_NO_EMPTY);
+  $parts = preg_split('/\?/', $_SERVER['REQUEST_URI'], -1, PREG_SPLIT_NO_EMPTY);
 
   $uri = trim($parts[0], '/');
-  $uri = (!config('rewrite') ? preg_replace('/^index\.php\/?/', '', $uri) : $uri);
   $uri = strlen($uri) ? $uri : 'index';
 
-  if ($fake_method !== null)
-    route($fake_method, "/{$uri}");
-  else
-    route(method(), "/{$uri}");
+  route(method(), "/{$uri}");
 }
 ?>
