3a7b2d9e47e20520e5a18d0632d6c4b24bde9bf1
Lars <borchert.lars@googlemail.com>
2013-05-21 14:49:06 +0300

    load more than one config file
    loaded config file will be merged with other already loaded config files

--- dispatch.php
+++ dispatch.php
@@ -91,7 +91,7 @@ function config($key, $value = null) {
   static $_config = array();
 
   if ($key === 'source' && file_exists($value))
-    $_config = parse_ini_file($value, true);
+    $_config = array_merge($_config, parse_ini_file($value, true));
   else if ($value == null)
     return (isset($_config[$key]) ? $_config[$key] : null);
   else
