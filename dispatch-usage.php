ab3d7f4dc31d77c330929c85f388ba1022f70c7c
Jesus A. Domingo <jesus.domingo@gmail.com>
2013-06-01 21:02:46 +0800

    various improvements, better restify(), error(), multiple filters()

---
+++ dispatch-usage.php
@@ -0,0 +1,126 @@
+<?php
+error_reporting(E_ALL|E_STRICT);
+
+include './dispatch.php';
+
+config('rewrite.enable', true);
+
+// some sample routes
+get('/index', function () {
+  echo "GET index\n";
+});
+
+post('/index', function () {
+  echo "POST index\n";
+});
+
+put('/index', function () {
+  echo "PUT index\n";
+});
+
+delete('/index', function () {
+  echo "DELETE index\n";
+});
+
+// let's call those routes
+dispatch('GET', '/index');
+dispatch('POST', '/index');
+dispatch('PUT', '/index');
+dispatch('DELETE', '/index');
+
+// a sample class that will be published
+// as a RESTful resource with all actions
+class Users {
+  public function onIndex() {
+    echo "Users::onIndex\n";
+  }
+  public function onNew() {
+    echo "Users::onNew\n";
+  }
+  public function onCreate() {
+    echo "Users::onCreate\n";
+  }
+  public function onShow($id) {
+    echo "Users::onShow {$id}\n";
+  }
+  public function onEdit($id) {
+    echo "Users::onEdit {$id}\n";
+  }
+  public function onUpdate($id) {
+    echo "Users::onUpdate {$id}\n";
+  }
+  public function onDelete($id) {
+    echo "Users::onDelete {$id}\n";
+  }
+}
+
+// a sample class that will be published
+// as a RESTful resource with just 2 actions
+class Pages {
+  public function onIndex() {
+    echo "Pages::onIndex\n";
+  }
+  public function onShow($id) {
+    echo "Pages::onShow {$id}\n";
+  }
+}
+
+// sample middleware, gets called per request
+middleware(function () {
+  echo "executing middleware [1] before request\n";
+});
+
+middleware(function () {
+  echo "executing middleware [2] before request\n";
+});
+
+// some route filters, gets called when a matching
+// route contains the given symbol (in this case, :id)
+filter('id', function ($id) {
+  echo "caught {$id} as :id\n";
+});
+
+filter('id', function ($id) {
+  echo "also caught {$id} as :id\n";
+});
+
+// publish instances of the two classes
+// as RESTful resources
+restify('/users', new Users());
+
+// another resource, but with just two actions
+restify('/pages', new Pages(), array('index', 'show'));
+
+// let's call those REST endpoints
+dispatch('GET', '/users/index');
+dispatch('GET', '/users/new');
+dispatch('POST', '/users/create');
+dispatch('GET', '/users/1');
+dispatch('GET', '/users/1/edit');
+dispatch('PUT', '/users/1');
+dispatch('DELETE', '/users/1');
+
+// this resource only supports two actions,
+// other actions will return 404s
+dispatch('GET', '/pages/');
+dispatch('GET', '/pages/1');
+
+// if you don't have mod_rewrite, do this
+config('rewrite.enable', false);
+
+// index.php/ will be stripped from the request URIs
+dispatch('GET', '/index.php/users');
+
+// some custom 404 error hooks
+error(404, function () {
+  echo "Nyargh!\n";
+});
+
+// you can have multiple error handlers
+error(404, function () {
+  echo "Oops!\n";
+});
+
+// let's invoke a 404
+dispatch('GET', '/does/not/exist');
+?>
