c669a6e1ff3520686cc3e2d8d98f10026c3bb075
noodlehaus <noodlehaus@gmail>
2013-07-19 16:19:18 +0800

    show stacking handlers for error()

--- examples/errors/errors.php
+++ examples/errors/errors.php
@@ -2,7 +2,11 @@
 include '../../src/dispatch.php';
 
 error(404, function () {
-  echo "Sorry, page not found!";
+  echo "Sorry, page not found!<br>";
+});
+
+error(404, function () {
+  echo "Sorry, we really can't find your page!<br>";
 });
 
 dispatch();
