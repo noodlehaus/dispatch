1994ceeaacd54dda5462defd9b577e7f58fa0c26
Jesus A. Domingo <jesus.domingo@gmail.com>
2013-07-19 16:18:07 +0800

    example shows stacking filters

--- examples/filters/filters.php
+++ examples/filters/filters.php
@@ -5,16 +5,29 @@ before(function () {
   echo "before()<br>";
 });
 
+before(function () {
+  echo "before() again<br>";
+});
+
 after(function () {
   echo "after()<br>";
 });
 
+after(function () {
+  echo "after() again<br>";
+});
+
 filter('name', function ($name) {
-  scope('greeting', "Hello there, {$name}!");
+  scope('greeting', "Hello there, {$name}!<br>");
+});
+
+filter('name', function ($name) {
+  scope('farewell', "Bye bye, {$name}!<br>");
 });
 
 on('GET', '/greet/:name', function ($name) {
-  echo scope('greeting')."<br>";
+  echo scope('greeting');
+  echo scope('farewell');
 });
 
 dispatch();
