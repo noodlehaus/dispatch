24b83da962637cac5ea30e46c79ab57c6c46ded2
noodlehaus <noodlehaus@gmail>
2013-08-01 11:28:09 +0800

    routes example

---
+++ examples/routes/routes.php
@@ -0,0 +1,25 @@
+<?php
+include '../../src/dispatch.php';
+
+on('GET', '/index', function () {
+  echo "GET /index invoked";
+});
+
+on('POST', '/index', function () {
+  echo "POST /index invoked";
+});
+
+on('PUT', '/index', function () {
+  echo "PUT /index invoked";
+});
+
+on('DELETE', '/index', function () {
+  echo "DELETE /index invoked";
+});
+
+on('HEAD', '/index', function () {
+  echo "HEAD /index invoked";
+});
+
+dispatch();
+?>
