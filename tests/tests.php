ff58285406b67d0c5599da9d9c47e10f400b3bb7
noodlehaus <noodlehaus@gmail>
2013-09-03 11:08:18 +0800

    improved docs on sample config, and tests

--- tests/tests.php
+++ tests/tests.php
@@ -10,15 +10,12 @@ start_http('0.0.0.0', '1234', 'app.php');
  * local tests
  */
 
-test('config setting and getting', function () {
+test('config()', function () {
   config('one', 1);
   config('false', false);
   assert(config('one') === 1);
   assert(config('false') === false);
   assert(config('invalid') === null);
-});
-
-test('config setting using an array', function () {
   config([
     'name' => 'noodlehaus',
     'project' => 'dispatch'
@@ -27,22 +24,22 @@ test('config setting using an array', function () {
   assert(config('project') === 'dispatch');
 });
 
-test('site path setting and getting', function () {
+test('site()', function () {
   config('dispatch.url', 'http://localhost:8888/mysite/');
   assert(site() === 'http://localhost:8888/mysite/');
   assert(site(true) === '/mysite');
 });
 
-test('url encoding', function () {
+test('u()', function () {
   $s = 'name=noodlehaus&project=dispatch';
   assert(u($s) === urlencode($s));
 });
 
-test('html escaping', function () {
+test('h()', function () {
   assert(h('&') === '&amp;');
 });
 
-test('cross-scope values', function () {
+test('scope()', function () {
   scope('one', 1);
   call_user_func(function () {
     assert(scope('one') === 1);
@@ -53,49 +50,49 @@ test('cross-scope values', function () {
  * remote tests
  */
 
-test('before routines', function () {
+test('before()', function () {
   $res = curly('GET', URL.'/index?name=dispatch');
   assert(preg_match('/BEFORE METHOD: GET/', $res));
   assert(preg_match('/BEFORE PATH: \/index/', $res));
 });
 
-test('after routines', function () {
+test('after()', function () {
   $res = curly('GET', URL.'/index?name=dispatch');
   assert(preg_match('/AFTER METHOD: GET/', $res));
   assert(preg_match('/AFTER PATH: \/index/', $res));
 });
 
-test('error triggers', function () {
+test('error(code)', function () {
   $res = curly('GET', URL.'/error');
   assert(preg_match('/500 page error/i', $res));
 });
 
-test('custom error handler', function () {
+test('error(code, callable)', function () {
   $res = curly('GET', URL.'/not-found');
   assert(preg_match('/file not found/', $res));
 });
 
-test('GET handler', function () {
+test('on(GET)', function () {
   $res = curly('GET', URL.'/index?name=dispatch');
   assert(preg_match('/GET received dispatch and dispatch/', $res));
 });
 
-test('POST handler', function () {
+test('on(POST)', function () {
   $res = curly('POST', URL.'/index', ['name' => 'dispatch']);
   assert(preg_match('/POST received dispatch and dispatch/i', $res));
 });
 
-test('PUT handler', function () {
+test('on(PUT)', function () {
   $res = curly('PUT', URL.'/index', ['name' => 'dispatch']);
   assert(preg_match('/PUT received dispatch/i', $res));
 });
 
-test('DELETE handler', function () {
+test('on(DELETE)', function () {
   $res = curly('DELETE', URL.'/index/1');
   assert(preg_match('/DELETE route test/i', $res));
 });
 
-test('POST file upload', function () {
+test('upload()', function () {
 
   if (PHP_VERSION_ID < 50500)
     $att = '@'.__DIR__.'/upload.txt';
@@ -106,12 +103,9 @@ test('POST file upload', function () {
   assert(preg_match('/received upload\.txt/', $res));
 });
 
-test('method override via _method', function () {
+test('method override (_method, X-HTTP-Method-Override)', function () {
   $res = curly('POST', URL.'/override', ['_method' => 'PUT']);
   assert(preg_match('/PUT received via _method/i', $res));
-});
-
-test('method override via X-HTTP-Method-Override', function () {
   $res = curly(
     'POST',
     URL.'/override',
@@ -121,76 +115,70 @@ test('method override via X-HTTP-Method-Override', function () {
   assert(preg_match('/PUT received via _method/i', $res));
 });
 
-test('json output', function () {
+test('json_out()', function () {
   $res = curly('GET', URL.'/json');
   $val = '{"name":"noodlehaus","project":"dispatch"}';
   assert(preg_match('/application\/json/', $res));
   assert(preg_match('/'.preg_quote($val).'/', $res));
-});
-
-test('jsonp output', function () {
   $res = curly('GET', URL.'/jsonp');
   $val = 'callback({"name":"noodlehaus","project":"dispatch"})';
   assert(preg_match('/application\/javascript/', $res));
   assert(preg_match('/'.preg_quote($val).'/', $res));
 });
 
-test('302 redirect (default)', function () {
+test('redirect()', function () {
   $res = curly('GET', URL.'/redirect/302');
   assert(preg_match('/302 found/i', $res));
   assert(preg_match('/Location: \/index/i', $res));
-});
-
-test('301 redirect', function () {
   $res = curly('GET', URL.'/redirect/301');
   assert(preg_match('/301 moved permanently/i', $res));
   assert(preg_match('/Location: \/index/i', $res));
 });
 
-test('route filter', function () {
+test('filter()', function () {
   $res = curly('GET', URL.'/index/123');
   assert(preg_match('/id found/i', $res));
   assert(preg_match('/id = 123/i', $res));
 });
 
-test('cookie setting', function () {
+test('cookie()', function () {
   $res = curly('GET', URL.'/cookie-set');
   assert(preg_match('/set-cookie: cookie=/i', $res));
   $res = curly('GET', URL.'/cookie-get');
   assert(preg_match('/cookie=123/i', $res));
 });
 
-test('session setting', function () {
+test('session()', function () {
   curly('GET', URL.'/session/setup');
   $res = curly('GET', URL.'/session/check');
   assert(preg_match('/i am dispatch/i', $res));
   assert(!preg_match('/type is still set/i', $res));
 });
 
-test('params fetching', function () {
+test('params()', function () {
   $res = curly('GET', URL.'/params?one=1&two=2');
   assert(preg_match('/one=1/', $res));
   assert(preg_match('/two=2/', $res));
 });
 
-test('flash messages', function () {
+test('flash()', function () {
   curly('GET', URL.'/flash-set');
   $res = curly('GET', URL.'/flash-get');
   assert(preg_match('/message=success/i', $res));
 });
 
-test('partials', function () {
+test('partial()', function () {
   $res = curly('GET', URL.'/partial/dispatch');
   assert(preg_match('/dispatch is awesome/', $res));
 });
 
-test('template rendering', function () {
+test('render()', function () {
   $res = curly('GET', URL.'/template/dispatch');
   assert(preg_match('/<!doctype html>/i', $res));
   assert(preg_match('/dispatch is awesome/', $res));
 });
 
-test('grouped routes', function () {
+test('resource()', function () {
   $res1 = curly('GET', URL.'/books/list');
   $res2 = curly('GET', URL.'/books/chapters/list');
   $res3 = curly('GET', URL.'/list');
