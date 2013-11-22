e28f4876d706fd24f246187d90ed447fea846f3f
Lloyd Zhou <lloydzhou@qq.com>
2013-11-22 15:28:04 +0800

    add helper function to get path. the code just from function "dispatch".

--- src/dispatch.php
+++ src/dispatch.php
@@ -832,7 +832,32 @@ function on($method, $path, $callback = null) {
       error(404, 'Page not found');
   }
 }
+/**
+ * helper function to get the pathinfo.
+ * move from function "dispatch"
+ */
+function path() {
+  
+  static $path;
+  
+  if (!$path) {
 
+    // normalize routing base, if site is in sub-dir
+    $path = parse_url($path ? $path : $_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $root = config('dispatch.router');
+    $base = site(true);
+  
+    // strip base from path
+    if ($base !== null)
+      $path = preg_replace('@^'.preg_quote($base).'@', '', $path);
+  
+    // if we have a routing file (no mod_rewrite), strip it from the URI
+    if ($root)
+      $path = preg_replace('@^/?'.preg_quote(trim($root, '/')).'@i', '', $path);
+  }
+  
+  return $path;
+}
 /**
  * Entry point for the library.
  *
@@ -846,19 +871,8 @@ function dispatch($method = null, $path = null) {
   // see if we were invoked with params
   $method = ($method ? $method : $_SERVER['REQUEST_METHOD']);
 
-  // normalize routing base, if site is in sub-dir
-  $path = parse_url($path ? $path : $_SERVER['REQUEST_URI'], PHP_URL_PATH);
-  $root = config('dispatch.router');
-  $base = site(true);
-
-  // strip base from path
-  if ($base !== null)
-    $path = preg_replace('@^'.preg_quote($base).'@', '', $path);
-
-  // if we have a routing file (no mod_rewrite), strip it from the URI
-  if ($root)
-    $path = preg_replace('@^/?'.preg_quote(trim($root, '/')).'@i', '', $path);
-
+  // move the code to a function.
+  $path = path();
   // check for override
   $override = request_headers('x-http-method-override');
   $override = $override ? $override : params('_method');
