8c6259ce338f97778578662d68dca1984dc8ee9f
noodlehaus <jesus.domingo@gmail.com>
2016-02-02 23:02:01 +0800

    pull in pico again, change response() to return callable

--- README.markdown
+++ README.markdown
@@ -35,8 +35,7 @@ $db = createDBConnection($config['db']);
 dispatch($db, $config);
 ```
 
-If you want to see where all the state goes, you can use `action`, `serve`,
-and `render` instead.
+If you want to see where all the state goes, you can use `action` and `serve`.
 
 ```php
 <?php
@@ -67,10 +66,10 @@ $verb = $_SERVER['REQUEST_METHOD'],
 $path = $_SERVER['REQUEST_URI'],
 
 # serve app against verb + path, pass dependencies
-list($content, $status, $headers) = serve($routes, $verb, $path, $db, $config);
+$responder = serve($routes, $verb, $path, $db, $config);
 
-# flush out content, status code, and http headers
-render($content, $status, $headers);
+# invoke responder to flush response
+$responder();
 ```
 
 ## tests
@@ -81,53 +80,7 @@ Tests for `dispatch` use plain assertions.
 php tests/dispatch-tests.php
 ```
 
-If no errors were echoed, then you're all good.
-
-## breaking changes from 9.x
-
-```
-noodlehaus/dispatch package name
-```
-
-## breaking changes from 8.x
-
-```
-removed the following:
-- attachments()
-- blanks()
-- config()
-- cookies()
-- ent()
-- error()
-- headers()
-- hook()
-- input()
-- ip()
-- json()
-- map()
-- nocache()
-- session()
-- stash()
-- status()
-- url()
-
-added the following:
-- action() - creates a route handler
-- response() - returns a response tuple
-- match() - matches the method + pattern against a list of actions
-- serve() - performs match() against actions list, and invokes the
-  match, and returns the resulting response tuple
-- page() - creates an action that renders the file, like inline phtml()
-- render() - renders the http response
-- route() - creates an action and puts it into the context singleton
-  (replaces the old map function)
-- context() - returns the context singleton
-
-changed the following:
-- redirect() - now just returns a redirect response tuple
-- phtml() - now requires the full path (minus extension) of phtml file,
-  and no longer accepts a layout file
-```
+If no errors were printed, then you're all good.
 
 ## license
 
--- tests/dispatch-tests.php
+++ tests/dispatch-tests.php
@@ -1,18 +1,69 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../dispatch.php';
 
+test_response();
 test_redirect();
+test_action();
+test_match();
+test_serve();
 test_context();
 test_phtml();
 test_page();
 test_route();
 test_dispatch();
 
+# response()
+function test_response() {
+  is_callable(response('foo', 200, ['content-type' => 'text/plain']));
+}
+
 # redirect()
 function test_redirect() {
-  assert(redirect('/index') === ['', 302, ['location' => '/index']]);
-  assert(redirect('/index', 301) === ['', 301, ['location' => '/index']]);
+  assert(is_callable(redirect('/index')));
+  assert(is_callable(redirect('/index', 301)));
+}
+
+# action()
+function test_action() {
+  $f1 = action('GET', '/index', function () {
+    return 'index';
+  });
+  $f2 = action('GET', '/:name/:location', function ($args) {
+    return $args['name'];
+  });
+  assert(is_callable($f1) && is_callable($f2));
+  assert(
+    empty($f1('POST', '/index')) &&
+    empty($f1('GET', '/about')) &&
+    empty($f2('POST', '/about')) &&
+    empty($f2('GET', '/about/bleh/moo'))
+  );
+  list($c1, $v1) = $f1('GET', '/index');
+  list($c2, $v2) = $f2('GET', '/dispatch/singapore');
+  assert($c1() === 'index' && empty($v1));
+  assert($c2($v2) === 'dispatch' && isset($v2['name'], $v2['location']));
+}
+
+# match()
+function test_match() {
+  $v = [
+    action('GET', '/index', function () { return 'GET index'; }),
+    action('POST', '/index', function () { return 'POST index'; })
+  ];
+  assert(empty(match($v, 'GET', '/about')));
+  list($f, $c) = match($v, 'POST', '/index');
+  assert(empty($c) && $f() === 'POST index');
+}
+
+# serve()
+function test_serve() {
+  $v = [
+    action('GET', '/index', function () { return response('GET index'); }),
+    action('POST', '/index', function ($d) { return response("POST {$d}"); })
+  ];
+  $r = serve($v, 'POST', '/index', 'foo');
+  assert(is_callable($r));
 }
 
 # context()
@@ -34,7 +85,7 @@ function test_phtml() {
 # page()
 function test_page() {
   $f = page(__DIR__.'/test-phtml', ['name' => 'world']);
-  assert(is_callable($f) && $f() === response('hello, world!', 200, []));
+  assert(is_callable($f) && is_callable($f()));
 }
 
 # route()
@@ -46,9 +97,7 @@ function test_route() {
   $m = $c[0];
   assert(is_callable($m));
   list($f, $v) = $m('GET', '/index');
-  assert(empty($v) && $f() === response(
-    'hello world!', 201, ['X-Custom-Value' => 'foo']
-  ));
+  assert(empty($v) && is_callable($f()));
 }
 
 # dispatch() - minus header and status check
